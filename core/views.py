6abaa3c998bd998c0a2d8f5b66045cf6157577f2
Miguel Angel Zamora Carmona <maz191099@gmail.com>
2020-05-31 15:32:42 -0500

    Commit despues de inicializar la app Pages

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def home(request):
-    return render(request, "core/home.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title': "Mi super Web Playgound"})
 
-def sample(request):
-    return render(request, "core/sample.html")
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
