6651cd1bda3cada1ab0b376fccc84d7312ea650c
Miguel Angel Zamora Carmona <maz191099@gmail.com>
2020-05-31 19:08:02 -0500

    Commit hecho despues de crear el mixin de pages

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,15 @@ from .models import Page
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from .forms import PageForm
+from django.shortcuts import redirect
+
+class StaffRequiredMixin(object):
+    # este mixin pedira que el usuario sea miembro del staff
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+
 # Create your views here.
 
 class PageListView(ListView):
@@ -11,19 +20,20 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     model = Page
 
-class PageCreate(CreateView):
+class PageCreate(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm
     success_url =  reverse_lazy('pages:pages')
+
     
-class PageUpdate(UpdateView):
+class PageUpdate(StaffRequiredMixin, UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = '_update_form'
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) +'?ok'
 
-class PageDelete(DeleteView):
+class PageDelete(StaffRequiredMixin, DeleteView):
     model = Page
     success_url =  reverse_lazy('pages:pages')
     
