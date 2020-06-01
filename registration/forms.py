7e4fef13d7de36816ad291f08435c6468a5b92fa
Miguel Angel Zamora Carmona <maz191099@gmail.com>
2020-06-01 13:58:08 -0500

    Commit hecho despues establecer el formulario de cambio email

--- registration/forms.py
+++ registration/forms.py
@@ -25,3 +25,16 @@ class ProfileForm(forms.ModelForm):
             'bio': forms.Textarea(attrs={'class':'form-control mt-3', 'rows':3, 'placeholder':'Biografía'}),
             'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
         }
+
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=True, help_text="Requerido, 254 caracteres como maximo y debe ser valido.")
+    class Meta:
+        model = User
+        fields = ['email']
+        
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if email in self.changed_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("El email ya está registrado, prueba con otro.")
+        return email
