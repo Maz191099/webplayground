9187fe09f5b7acf0cc5a4b6dda28e1e16dd59a7e
Miguel Angel Zamora Carmona <maz191099@gmail.com>
2020-06-01 23:35:52 -0500

    Commit hecho despues establecer los templates de messenger

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from pages.urls import pages_patterns
 from django.conf import settings
 from profiles.urls import profiles_patterns
+from messenger.urls import messenger_patterns
 
 urlpatterns = [
     path('', include('core.urls')),
@@ -28,6 +29,8 @@ urlpatterns = [
     path('accounts/', include('registration.urls')),
     # Paths de profiles
     path('profiles/', include(profiles_patterns)),
+    # Paths de messenger
+    path('messenger/', include(messenger_patterns)),
 ]
 if settings.DEBUG:
     from django.conf.urls.static import static
